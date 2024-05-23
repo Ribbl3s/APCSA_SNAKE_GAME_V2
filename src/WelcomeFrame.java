632615519004517c37be9f328975846a13f27c0c
RIbbl3s <bennyl32@nycstudents.net>
2024-05-23 13:00:35 -0400

    hi

---
+++ src/WelcomeFrame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class WelcomeFrame extends JPanel implements ActionListener {
+    private WelcomeFrame panel;
+    private JTextField textField;
+    private JButton submitButton;
+    private JButton clearButton;
+    private JFrame enclosingFrame;
+
+
+
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+
+}
